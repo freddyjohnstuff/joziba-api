7cf1b06325c52e08f453e029574d0b58e35a4c91
Dilshod Sanginov <dilshod.sanginov@auslogics.com>
2024-02-04 23:17:02 +1000

    Init

---
+++ migrations/m240204_125757_clients.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240204_125757_clients
+ */
+class m240204_125757_clients extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240204_125757_clients cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m240204_125757_clients cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
