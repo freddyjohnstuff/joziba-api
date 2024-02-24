7881ec168bd61c903f6f32dfcf2039fb2f221195
a.madzhidov <a.madzhidov@creonit.ru>
2024-02-24 12:22:07 +0200

    CORS

--- modules/v1/components/controller/BaseActiveController.php
+++ modules/v1/components/controller/BaseActiveController.php
@@ -46,6 +46,7 @@ class BaseActiveController extends ActiveController
                 'Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Origin' => ['*'],
                 'Access-Control-Allow-Credentials' => false,
                 'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => [
--- modules/v1/components/controller/BaseController.php
+++ modules/v1/components/controller/BaseController.php
@@ -43,6 +43,7 @@ class BaseController extends \yii\rest\Controller
                 'Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Origin' => ['*'],
                 'Access-Control-Allow-Credentials' => false,
                 'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => [
